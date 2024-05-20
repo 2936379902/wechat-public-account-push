7fae737db1f038841dac61fde49860d1f32fa2e5
2936379902 <2936379902@qq.com>
2024-05-20 20:52:20 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'LRguo9wnzDqL4_zVtXgBbJ9sNUfpa-VVUHh5DIVHlkU',
+  CALLBACK_TEMPLATE_ID: '',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oa1xG6ZjFt39gtMA4LH5J3E-Kbbk',
+      id: '',
     }
   ],
 
